88544a2538fc13edba19dd3ed307de06c9ec9c56
math4tots <math4tots@gmail.com>
2014-02-27 09:00:15 -0800

    simplify a lot of things

--- marble.py
+++ marble.py
@@ -1,23 +1,24 @@
 '''
-(marble) language
-
-Looks like lisp, but is semantically more like python or javascript.
+(marble)
 '''
 
+'''
+------------------------------------------------------------------------------
+PART 1 -----------------------------------------------------------------------
+------------------------------------------------------------------------------
+
+Parsing
+
+isntances of subclasses of `Token` are essentially just one of the immutable
+builtin Python types with some extra data
+
+parse function takes a string and returns a `list` of instances of subclasses
+of `Token` with all the proper metadata filled in.
+
+'''
+from sys import stdout, stdin
+
 class Token(object):
-	'''
-	Construct resulting from parse
-
-	Initially I wanted to mix both mutable and immutable Python types.
-	However, I realized that depending on whether I wanted to mix with
-	immutable or mutable, it was better to leave __new__ or __init__ 
-	unimplemented.
-
-	So I have instead decided to require that any type mixing in with Token
-	must be immutable (so instead of with list, I mix with tuple).
-
-	This way I don't touch __init__ but only override __new__
-	'''
 	def __new__(cls,text,begin,end,*args,**kargs):
 		self = super(Token,cls).__new__(cls,*args,**kargs)
 		self.text = text
@@ -25,236 +26,159 @@ class Token(object):
 		self.end = end
 		return self
 
-class Tuple(Token,tuple):
-	'''
-	Tuple
-	Used as function call in code.
-	'''
-	def evaluate(self,env):
-		env.root.call_stack.append(self)
-		try:
-			return self[0].evaluate(env)(env,*self[1:])
-		finally:
-			env.root.call_stack.pop()
-
-class Str(Token,str):
-	'''
-	String
-	Used as variable name/symbol in code.
-	'''
-	def evaluate(self,env):
-		return env[self]
-
-class Qstr(Token,str):
-	'''
-	Quoted string
-	'''
-	def evaluate(self,env):
-		return self
-
-class Int(Token,int):
-	def evaluate(self,env):
-		return self
-
-class Float(Token,float):
-	def evaluate(self,env):
-		return self
-
-class Environment(object):
-	def __init__(self,parent):
-		self.parent = parent
-		self.table = dict()
-		if not self.parent:
-			self.call_stack = []
-			self.root = self
-		else:
-			self.root = parent.root
-
-	def __getitem__(self,k):
-		if k in self.table:
-			return self.table[k]
-		elif self.parent:
-			return self.parent[k]
-		else:
-			raise KeyError(k)
-
-	def __setitem__(self,k,v):
-		if k in self.table:
-			self.table[k] = v
-		elif self.parent:
-			self.parent[k] = v
-		else:
-			raise KeyError(k)
-
-	def declare(self,k,v):
-		self.table[k] = v
+class Int(Token,int): pass
+class Float(Token,float): pass
+class Name(Token,str): pass
+class Str(Token,str): pass
+class Tuple(Token,tuple): pass
 
 def parse(s):
 	s += '\0'
 	t = [ [] ]
 	b = [    ]
-	l = len(s)
-	i = next(k for k in range(l) if not s[k].isspace())
-	while s[i] != '\0':
+	i = next(k for k in range(len(s)) if not s[k].isspace())
+	while s[i]!='\0':
 		j = i
-
 		if s[i] == '(':
-			b.append(j)
 			t.append([])
+			b.append(i)
 			i += 1
-
 		elif s[i] == ')':
 			i += 1
-			start = b.pop()
-			value = t.pop()
-			end = i
-			t[-1].append(Tuple(s,start,end,value))
-
+			t[-2].append(Tuple(s,b.pop(),i,t.pop()))
+		elif s[i] == '"':
+			i=next(
+				k for k in range(i+1,len(s))if s[k]=='"'and s[k-1]!='\\')+1
+			t[-1].append(Str(s,j,i,eval(s[j:i])))
 		else:
-			if s[i] == '"':
-				i = next(k for k in range(i+1,l) if s[k] == '"' and s[k-1]!='\\') + 1
-				t[-1].append(Qstr(s,j,i,eval(s[j:i])))
-
-			else:
-				i = next(k for k in range(i,l) if s[k].isspace() or s[k] in '()"\0')
-
+			i=next(
+				k for k in range(i,len(s))if s[k]in'"()\0'or s[k].isspace())
+			x = s[j:i]
+			try:
+				x = Int(s,j,i,x)
+			except ValueError:
 				try:
-					t[-1].append(Int(s,j,i,s[j:i]))
+					x = Float(s,j,i,x)
 				except ValueError:
-					try:
-						t[-1].append(Float(s,j,i,s[j:i]))
-					except ValueError:
-						t[-1].append(Str(s,j,i,s[j:i]))
-
-		i = next(k for k in range(i,l) if not s[k].isspace())
-	assert len(t) == 1, t
+					x = Name(s,j,i,x)
+			t[-1].append(x)
+		i = next(k for k in range(i,len(s)) if not s[k].isspace())
 	return t[0]
-
-root_env = Environment(None)
-
-root_env.declare('none',None)
-root_env.declare('true',True)
-root_env.declare('false',False)
-
-def execute(xs,env = root_env):
-	for x in xs:
-		x.evaluate(env)
-
-def to_function(f):
-	'''
-	helper function that turns given python function to act like a function
-	'''
-	def function(env,*args):
-		return f(*[x.evaluate(env) for x in args])
-	return function
-
-def to_string(x):
-	if x is None:
-		x = 'none'
-	elif isinstance(x,bool):
-		'''
-		Something I tripped up on at first, is that
-		bool is a subclass of int. So this test has to come first. 
-		'''
-		x = 'true' if x else 'false'
-	elif isinstance(x,(int,float,str)):
-		x = str(x)
-	elif isinstance(x,tuple):
-		x = '(' + ' '.join(map(to_string,x)) + ')'
-	elif isinstance(x,Environment):
-		x = '<environment>'
-	elif callable(x):
-		x = '<callable>'
-	else:
-		raise TypeError(x)
-	return x
-root_env.declare('str',to_function(to_string))
-
-def print_(x):
-	print(to_string(x))
-	return x
-root_env.declare('print',to_function(print_))
-
-def quote(env,x):
-	return x
-root_env.declare('quote',quote)
-
-def get_current_environment(env):
-	return env
-root_env.declare('get-current-environment', get_current_environment)
-
-def make_macro(env,env_name,arg_names,body):
-	def macro(ienv,*args):
-		if len(arg_names) != len(args):
-			raise TypeError('expected %d argument(s), got %d' % (len(arg_names),len(args)))
-		nenv = Environment(env)
-		nenv.declare(env_name,ienv)
-		for name, arg in zip(arg_names,args):
-			nenv.declare(name,arg)
-		return body.evaluate(nenv)
-	return macro
-root_env.declare('macro',make_macro)
-
-def declare(env,name,value):
-	value = value.evaluate(env)
-	env.declare(name,value)
-	return value
-root_env.declare('declare',declare)
-
-def get_item(xs,i):
-	return xs[i]
-root_env.declare('get-item',to_function(get_item))
-
-def length(x):
-	if isinstance(x,(list,tuple)):
-		return len(x)
-	else:
-		raise TypeError('')
-root_env.declare('length',to_function(length))
-
-def while_(env,condition,body):
-	while condition.evaluate(env):
-		last = body.evaluate(env)
-	return last
-root_env.declare('while',while_)
-
-def less_than(a,b):
-	return a < b
-root_env.declare('<',to_function(less_than))
-
-def do(env,expressions):
-	for expression in expressions:
-		last = expression.evaluate(env)
-	return last
-root_env.declare('do',do)
-
-def assign(env,x,e):
-	if isinstance(x,str):
-		env[x] = e.evaluate(env)
-	else:
-		raise TypeError(x)
-root_env.declare('=',assign)
-
-root_env.declare('+',to_function(lambda a, b : a + b))
-root_env.declare('-',to_function(lambda a, b : a - b))
-root_env.declare('*',to_function(lambda a, b : a * b))
-root_env.declare('/',to_function(lambda a, b : a / b))
-root_env.declare('//',to_function(lambda a, b : a // b))
-
-
-s = '''
-(declare i 1)
-(declare xs (quote (0 1 2 3 4 5 6 7 8 9 10)))
-
-(while (< i 10)
-	(do (
-		(print (+ "hey! " (str i)))
-		(print (get-item xs i))
-		(= i (+ i i))
-	))
-)
 
 
 '''
-execute(parse(s))
+------------------------------------------------------------------------------
+PART 2 -----------------------------------------------------------------------
+------------------------------------------------------------------------------
 
+builtin functions
+
+In this PART we define functions that are going to be callable from the
+(marble) langauge.
+
+'''
+
+def function(f):
+	return lambda env, *args : f(*[evaluate(env,x) for x in args])
+
+def evaluate(env,x):
+	if isinstance(x,(Int,Float,Str)):
+		return x
+	elif isinstance(x,Name):
+		x = str(x)
+		while x not in env:
+			try:
+				env = env['__parent__']
+			except KeyError:
+				raise KeyError(x)
+		return env[x]
+	elif isinstance(x,Tuple):
+		return evaluate(env,x[0])(env,*x[1:])
+	else:
+		raise Exception()
+
+def execute(env,xs):
+	e = {'__parent__' : env}
+	for x in xs:
+		evaluate(e,x)
+
+def run(env,s):
+	execute(env,parse(s))
+
+def str_(x):
+	return (
+		'none'  if x is None else
+		'true'  if x is True else
+		'false' if x is False else
+		str(x))
+
+def repr_(x):
+	return (
+		'none'  if x is None else
+		'true'  if x is True else
+		'false' if x is False else
+		str(x)  if isinstance(x,Name) else
+		'('+' '.join(map(repr_,x))+')' if isinstance(x,Tuple) else
+		repr(x))
+
+def print_(x):
+	print(str_(x))
+
+def quote(env,x):
+	return x
+
+def declare(env,name,value):
+	value = evaluate(env,value)
+	env[str(name)] = value
+	return value
+
+def assign(env,name,value):
+	name =str(name)
+	value = evaluate(env,value)
+	while name not in env:
+		try:
+			env = env['__parent__']
+		except KeyError:
+			raise KeyError(name)
+	env[name] = value
+	return value
+
+def lambda_(env,argnames,body):
+	def lambda_instance(ienv,*args):
+		if len(argnames) != len(args):
+			raise TypeError((len(argnames),argnames,args))
+		nenv = {name:evaluate(ienv,arg) for name, arg in zip(argnames,args)}
+		nenv['__parent__'] = env
+	return lambda_instance
+
+env = {
+	'none'    : None,
+	'true'    : True,
+	'false'   : False,
+	'eval'    : function(evaluate),
+	'str'     : function(str_),
+	'repr'    : function(repr_),
+	'print'   : function(print_),
+	'quote'   : quote,
+	'declare' : declare,
+	'='       : assign,
+	'+'       : function(lambda a, b: a + b),
+	'-'       : function(lambda a, b: a - b),
+	'*'       : function(lambda a, b: a * b),
+	'/'       : function(lambda a, b: a / b),
+	'//'      : function(lambda a, b: a // b),
+	'**'      : function(lambda a, b: a ** b),
+	'<'       : function(lambda a, b: a <  b),
+	'<='      : function(lambda a, b: a <= b),
+	'>'       : function(lambda a, b: a >  b),
+	'>='      : function(lambda a, b: a >= b),
+	'[]'      : function(lambda a, b: a[b])
+}
+
+run(env,'''
+(declare x 12)
+(= x 33)
+(print x)
+(print (quote "hello world!"))
+(print "hello world!")
+''')
